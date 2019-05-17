d44e10751b5bae101171df5df52b1f5b341c919e
Richard Batista <rbatista@nyccfb.info>
2019-05-17 14:54:38 -0400

    update

---
+++ master/app/models/context.js
@@ -0,0 +1,12 @@
+
+const master = require('masterrecord');
+
+class Context extends master.Context{
+    constructor() {
+        super();
+        // this.dbset(YOURMODEL);
+    }
+
+}
+
+module.exports = Context
