7352481b58697a206abdb799a926a16ccf0c00a3
Richard Batista <rbatista@nyccfb.info>
2019-06-27 17:50:17 -0400

    update

--- component/component.js
+++ component/component.js
@@ -5,7 +5,7 @@ module.exports = function(requestObject) {
     
     master.masterRoot = requestObject.masterRoot;
     master.root = requestObject.root;
-    master.require(["MasterError", "MasterRouter", "MasterView", "MasterHtml", "MasterTemp" , "MasterAction", "MasterActionFilters", "MasterSocket", "MasterJWT", "MasterSession"]);
+    master.require(["MasterError", "MasterTools", "MasterRouter", "MasterView", "MasterHtml", "MasterTemp" , "MasterAction", "MasterActionFilters", "MasterSocket", "MasterJWT", "MasterSession"]);
     require('./config/routes');
     require("./config/initializers/config");
     require("./config/load")(requestObject);
