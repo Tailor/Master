0c820f4ce432042ce53bf35e555fe879c9b1de0c
Richard Alexander Batista <rbatista111@gmail.com>
2022-09-22 23:00:07 -0400

    updaye

--- component/config/initializers/config.js
+++ component/config/initializers/config.js
@@ -1,13 +1,9 @@
 // Dont need to load everything main Config file loaded
 var master = require('mastercontroller');
-var root = __dirname;
-
-// initlaizing the tools we need for Master to run properly
-master.router.init(root);
 
 
-// register these apps to have access to them in the controller.
-  // example: master.register("mainContext", { anyobject : "name"});
+    // register these apps to have access to them in the controller.
+      // example: master.register("mainContext", { anyobject : "name"});
 
-// require as many components you need 
-  // example: require(`${master.root}/components/myComponent/initializers/config`);
+    // require as many components you need 
+      // example: master.component("components", "auth");
--- master/config/initializers/config.js
+++ master/config/initializers/config.js
@@ -3,22 +3,22 @@
     var mimes = require('./mime.json');
     var request = require('./request.json');
     var cors = require('./cors.json');
-    var root = __dirname;
 
     // initlaizing the tools we need for Master to run properly
     master.serverSettings(master.env.server);
     master.request.init(request);
     master.error.init(master.env.error);
     master.router.addMimeList(mimes);
-    master.router.init(root);
     master.socket.init();
     master.sessions.init();
     master.jwt.init().sha256();
     master.cors.init(cors);
+    master.startMVC("config");
+
 
     // register these apps to have access to them in the controller.
       // example: master.register("mainContext", { anyobject : "name"});
 
     // require as many components you need 
-      // example: require(`${master.root}/components/myComponent/initializers/config`);
+      // example: master.component("components", "auth");
 
